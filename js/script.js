10f30f23ef3bc9581ddc7a617d88bd39f523c8d8
tomasdarienko <darienkotomas@gmail.com>
2023-12-04 19:57:04 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
 
 
-let dipendenti =[
+let dipendenti = [
      {
-          nome : "wanye barnett",
-          ruolo : "CEO",
-          immagine : ".jpg"
+          nome: "wanye barnett",
+          ruolo: "CEO",
+          immagine: ".jpg"
      },
      {
-          nome : "Angela",
-          ruolo : "chief editor",
-          immagine : ".jpg"
+          nome: "Angela",
+          ruolo: "chief editor",
+          immagine: ".jpg"
      },
      {
-          nome : "walter",
-          ruolo : "office manager",
-          immagine : ".jpg"
+          nome: "walter",
+          ruolo: "office manager",
+          immagine: ".jpg"
      },
      {
-          nome : "Angela",
-          ruolo : "S.M.M",
-          immagine : ".jpg"
+          nome: "Angela",
+          ruolo: "S.M.M",
+          immagine: ".jpg"
      },
      {
-          nome : "scott",
-          ruolo : "dev",
-          immagine : ".jpg"
+          nome: "scott",
+          ruolo: "dev",
+          immagine: ".jpg"
      },
      {
-          nome : "barbara",
-          ruolo : "graph",
-          immagine : ".jpg"
+          nome: "barbara",
+          ruolo: "graph",
+          immagine: ".jpg"
      }
 ]
 
-for(let key in dipendenti){
-     console.log(dipendenti[key])
+for (let i = 0;i<dipendenti.length;i++) {
+     for (let key in dipendenti[i]) {
+          document.getElementById('dipendenti').innerHTML += `<li>${[key]} = ${dipendenti[i][key]}</li> `
+          console.log(dipendenti[key])
+     }
 }
+
