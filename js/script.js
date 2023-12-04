63ae8881537fa6551430f29b0b3fc6d41b02934d
tomasdarienko <darienkotomas@gmail.com>
2023-12-04 17:46:08 +0100

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+let dipendenti =[
+     {
+          nome : "wanye barnett",
+          ruolo : "CEO",
+          immagine : ".jpg"
+     },
+     {
+          nome : "Angela",
+          ruolo : "chief editor",
+          immagine : ".jpg"
+     },
+     {
+          nome : "walter",
+          ruolo : "office manager",
+          immagine : ".jpg"
+     },
+     {
+          nome : "Angela",
+          ruolo : "S.M.M",
+          immagine : ".jpg"
+     },
+     {
+          nome : "scott",
+          ruolo : "dev",
+          immagine : ".jpg"
+     },
+     {
+          nome : "barbara",
+          ruolo : "graph",
+          immagine : ".jpg"
+     }
+]
